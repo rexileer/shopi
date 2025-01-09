9516adc63f8fe25ad67031b6a22eca6fc2829632
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-10 00:27:25 +0300

    Update admin.py
    Shop admin Models

--- bigcorp/shop/admin.py
+++ bigcorp/shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent', 'slug']
+    ordering = ['name']
+    
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'brand', 'slug', 'price', 'available', 'created_at', 'updated_at']
+    list_filter = ['category', 'available', 'created_at', 'updated_at']
+    ordering = ['name']
+    
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
