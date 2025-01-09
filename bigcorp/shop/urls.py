78de2665df306317139af1b47dcbeaacb472a231
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-10 01:25:23 +0300

    shop urls

---
+++ bigcorp/shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+import views
+
+
+app_name = 'shop'
+
+urlpatterns = [
+
+    path('', views.products_view, name='products'),
+    path('<slug:slug>/', views.product_detail_view, name='product_detail'),
+    path('search/<slug:slug>/', views.category_list, name='category_list'),
+    
+]
