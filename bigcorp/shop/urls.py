ca146a5bd71fa94eff66cdefca2b7bffc29fc004
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-11 00:15:46 +0300

    Update urls.py
    fix

--- bigcorp/shop/urls.py
+++ bigcorp/shop/urls.py
@@ -6,7 +6,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.products_view, name='products'),
-    path('<slug:slug>/', views.product_detail_view, name='product_detail'),
-    path('search/<slug:slug>/', views.category_list, name='category_list'),
+    path('<slug:slug>/', views.product_detail_view, name='product-detail'),
+    path('search/<slug:slug>/', views.category_list, name='category-list'),
     
 ]
