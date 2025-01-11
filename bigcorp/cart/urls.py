3b5e1fef66272697e31bb6598d7c9581b93188ad
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-11 23:24:21 +0300

    Update urls.py

--- bigcorp/cart/urls.py
+++ bigcorp/cart/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    path('', views.cart_view, name='cart'),
+    path('', views.cart_view, name='cart-view'),
     path('add/<slug:slug>/', views.cart_add, name='cart-add'),
     path('delete/<slug:slug>/', views.cart_delete, name='cart-delete'),
     path('update/<slug:slug>/', views.cart_update, name='cart-update'),
