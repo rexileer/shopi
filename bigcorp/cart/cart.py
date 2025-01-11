bd5e84b44ca8273f468a1a09228e9f8afa289127
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-11 23:24:52 +0300

    Update cart.py
    class Cart (session_key)

---
+++ bigcorp/cart/cart.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+from shop.models import ProductProxy
+
+
+class Cart():
+    def __init__(self, request):
+        
+        self.session = request.session
+        
+        cart = self.session.get('session_key')
+        
+        if not cart:
+            cart = self.session['session_key'] = {}
+            
+        self.cart = cart
