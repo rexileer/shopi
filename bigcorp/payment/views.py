ada239334f4ed6949f4b518050441bd2afe09805
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-16 23:36:45 +0300

    Update views.py
    shipping, checkout, payment success/fail

--- bigcorp/payment/views.py
+++ bigcorp/payment/views.py
@@ -4,27 +4,27 @@ from django.http import JsonResponse
 
 from cart.cart import Cart
 
-from .forms import ShippingAdressForm
-from .models import ShippingAdress, Order, OrderItem
+from .forms import ShippingAddressForm
+from .models import ShippingAddress, Order, OrderItem
 
 
 @login_required(login_url='account:login')
 def shipping_view(request):
     try:
-        shipping_adress = ShippingAdress.objects.get(user=request.user)
-    except ShippingAdress.DoesNotExist:
-        shipping_adress = None
-        
-    form = ShippingAdressForm(instance=shipping_adress)
-    
+        shipping_address = ShippingAddress.objects.get(user=request.user)
+    except ShippingAddress.DoesNotExist:
+        shipping_address = None
+
+    form = ShippingAddressForm(instance=shipping_address)
+
     if request.method == 'POST':
-        form = ShippingAdressForm(request.POST, instance=shipping_adress)
+        form = ShippingAddressForm(request.POST, instance=shipping_address)
         if form.is_valid():
-            shipping_adress = form.save(commit=False)
-            shipping_adress.user = request.user
-            shipping_adress.save()
+            shipping_address = form.save(commit=False)
+            shipping_address.user = request.user
+            shipping_address.save()
             return redirect('account:dashboard')
-    
+
     context = {
         'form': form,
     }
@@ -32,14 +32,73 @@ def shipping_view(request):
 
 
 def checkout_view(request):
+    if request.user.is_authenticated:
+        shipping_address = get_object_or_404(ShippingAddress, user=request.user)
+        if shipping_address:
+            return render(request, 'payment/checkout.html', {'shipping_address': shipping_address})
     return render(request, 'payment/checkout.html')
 
 
 def complete_order_view(request):
-    return render(request, 'payment/complete-order.html')
+    if request.POST.get('action') == 'payment':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        country = request.POST.get('country')
+        city = request.POST.get('city')
+        street = request.POST.get('street')
+        apartment = request.POST.get('apartment')
+        zip = request.POST.get('zip')
+
+        cart = Cart(request)
+        total_price = cart.get_total_price()
+
+        shipping_address, _ = ShippingAddress.objects.get_or_create(
+            user=request.user,
+            defaults={
+                'full_name': name,
+                'email': email,
+                'country': country,
+                'city': city,
+                'street': street,
+                'apartment': apartment,
+                'zip': zip
+            }
+        )
+
+        if request.user.is_authenticated:
+            order = Order.objects.create(
+                user=request.user,
+                shipping_address=shipping_address,
+                amount=total_price
+            )
+
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    quantity=item['qty'],
+                    price=item['price'],
+                    user=request.user
+                )
+        else:
+            order = Order.objects.create(
+                shipping_address=shipping_address,
+                amount=total_price
+            )
+
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    quantity=item['qty'],
+                    price=item['price'],
+                )
+        return JsonResponse({'success': True})
 
 
 def payment_success_view(request):
+    for key in list(request.session.keys()):
+        del request.session[key]
     return render(request, 'payment/payment-success.html')
 
 
