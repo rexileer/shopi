a771ea6f0d54ee1c4e692614e3cb09750af452d9
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-16 22:03:52 +0300

    Update views.py
    shipping_view

--- bigcorp/payment/views.py
+++ bigcorp/payment/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render, get_object_or_404
+from django.http import JsonResponse
+
+from cart.cart import Cart
+
+from .forms import ShippingAdressForm
+from .models import ShippingAdress, Order, OrderItem
 
 
+@login_required(login_url='account:login')
 def shipping_view(request):
-    return render(request, 'payment/shipping.html')
+    try:
+        shipping_adress = ShippingAdress.objects.get(user=request.user)
+    except ShippingAdress.DoesNotExist:
+        shipping_adress = None
+        
+    form = ShippingAdressForm(instance=shipping_adress)
+    
+    if request.method == 'POST':
+        form = ShippingAdressForm(request.POST, instance=shipping_adress)
+        if form.is_valid():
+            shipping_adress = form.save(commit=False)
+            shipping_adress.user = request.user
+            shipping_adress.save()
+            return redirect('account:dashboard')
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'payment/shipping.html', context)
 
 
 def checkout_view(request):
