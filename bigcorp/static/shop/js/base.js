658984ed07a6039cb4ff1dcb11f82e1ea4fc299b
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-22 12:55:51 +0300

    collectstatic

---
+++ bigcorp/static/shop/js/base.js
@@ -0,0 +1,24 @@
+const date = new Date();
+document.querySelector(".year").innerHTML = date.getFullYear();
+
+setTimeout(function () {
+  $("#message").fadeOut("slow");
+}, 3000);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdowns = document.querySelectorAll('.nav-item.dropdown');
+    dropdowns.forEach(dropdown => {
+        dropdown.addEventListener('mouseenter', function () {
+            const menu = this.querySelector('.dropdown-menu');
+            const toggle = this.querySelector('.dropdown-toggle');
+            menu.classList.add('show');
+            toggle.setAttribute('aria-expanded', 'true');
+        });
+        dropdown.addEventListener('mouseleave', function () {
+            const menu = this.querySelector('.dropdown-menu');
+            const toggle = this.querySelector('.dropdown-toggle');
+            menu.classList.remove('show');
+            toggle.setAttribute('aria-expanded', 'false');
+        });
+    });
+});
