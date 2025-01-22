ac901cf2b40707182bca61e1c18fe1c739a592ee
rexileer <110720074+rexileer@users.noreply.github.com>
2025-01-22 12:55:03 +0300

    related_name on orderitem

--- bigcorp/payment/models.py
+++ bigcorp/payment/models.py
@@ -94,7 +94,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(
-        Order, on_delete=models.CASCADE, blank=True, null=True)
+        Order, on_delete=models.CASCADE, blank=True, null=True, related_name="items")
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
